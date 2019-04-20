4747ef9bb89a571e99152579da539883448846b7
Lindsay Xue <lindsay@Lingzis-MacBook-Pro.local>
2019-04-21 10:52:07 +1200

    Initialize Navbar & Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+// import Login from './components/auth/Login'
+// import Register from './components/auth/Register'
+import Dashboard from './components/dashboard/Dashboard'
+import User from './components/user/User'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/user" component={User} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import Logo from '../../assets/hifive.png'
+
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            <img
+              src={Logo}
+              width="30"
+              height="30"
+              class="d-inline-block align-top"
+              alt="Logo"
+            />{' '}
+            HiFIVE
           </Link>
           <button
             className="navbar-toggler"
@@ -17,6 +26,31 @@ class Navbar extends Component {
           >
             <span className="navbar-toggler-icon" />
           </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">
+                  {' '}
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/user">
+                  {' '}
+                  User
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="" className="nav-link">
+                  {' '}
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
