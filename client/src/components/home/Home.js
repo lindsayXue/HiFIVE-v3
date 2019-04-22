c7339875d80546eb8172c7e803e11e0545a1cc62
Lindsay Xue <lindsay@Lingzis-MacBook-Pro.local>
2019-04-23 11:44:08 +1200

    Journey

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home row d-flex justify-content-around">
-        <div className="card col-md-7 px-0 mt-2">
+        <div className="card col-md-6 px-0 mt-2">
           <Journey />
         </div>
         <div className="card col-md-4 px-0 mt-2">
--- client/src/components/home/Journey.js
+++ client/src/components/home/Journey.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import capeReinga from '../../assets/capeReinga.jpg'
+import capeReinga from '../../assets/capeReinga1.jpg'
 import auckland from '../../assets/auckland.jpg'
 import wellington from '../../assets/wellington.jpg'
 import christchurch from '../../assets/christchurch.jpg'
@@ -13,12 +13,12 @@ class Journey extends Component {
   state = {
     distanceNZ: 2122000
   }
-
+  ß
   render() {
     const { activity } = this.props
     const { distanceNZ } = this.state
 
-    let percent = (activity.points * 1000) / distanceNZ
+    let percent = ((activity.points * 1000) / distanceNZ).toFixed(2)
     if (percent > 100) {
       percent = 100
     }
@@ -27,45 +27,49 @@ class Journey extends Component {
       width: `${percent}%`
     }
 
+    const journeyImg = percent => {
+      if (percent >= 0 && percent < 20) {
+        return <img src={capeReinga} className="card-img-top" alt="city" />
+      }
+      if (percent >= 20 && percent < 40) {
+        return <img src={auckland} className="card-img-top" alt="city" />
+      }
+      if (percent >= 40 && percent < 60) {
+        return <img src={wellington} className="card-img-top" alt="city" />
+      }
+      if (percent >= 60 && percent < 80) {
+        return <img src={christchurch} className="card-img-top" alt="city" />
+      }
+      if (percent >= 80 && percent < 100) {
+        return <img src={dunedin} className="card-img-top" alt="city" />
+      }
+      if (percent === 100) {
+        return <img src={stirlingpoint} className="card-img-top" alt="city" />
+      }
+    }
+
     return (
       <div>
-        {percent >= 0 && percent < 20 ? (
-          <img src={capeReinga} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
-        {percent >= 20 && percent < 40 ? (
-          <img src={auckland} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
-        {percent >= 40 && percent < 60 ? (
-          <img src={wellington} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
-        {percent >= 60 && percent < 80 ? (
-          <img src={christchurch} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
-        {percent >= 80 && percent < 100 ? (
-          <img src={dunedin} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
-        {percent === 100 ? (
-          <img src={stirlingpoint} className="card-img-top" alt="city" />
-        ) : (
-          ''
-        )}
+        {journeyImg(percent)}
         <h5 className="card-header text-info">
-          Journey
+          {activity.points * 10} meters NOW!
           <small className="text-muted font-italic float-right">
             One point = Ten meters
           </small>
         </h5>
         <div className="card-body">
+          <div className="progress m-2">
+            <div
+              className="progress-bar progress-bar-striped bg-info"
+              role="progressbar"
+              style={progressStyle}
+              aria-valuenow={percent}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              {percent} %
+            </div>
+          </div>
           <div className="row d-flex justify-content-between">
             <div className="text-muted">
               <small>
@@ -132,18 +136,6 @@ class Journey extends Component {
                   Stirling Point
                 </i>
               </small>
-            </div>
-          </div>
-          <div className="progress">
-            <div
-              className="progress-bar progress-bar-striped bg-info"
-              role="progressbar"
-              style={progressStyle}
-              aria-valuenow="50"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >
-              {percent} %
             </div>
           </div>
         </div>
