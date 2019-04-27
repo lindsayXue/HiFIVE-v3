bdbed9d2c7db850069037d28318a92841a281351
Lindsay Xue <lindsay@Lingzis-MacBook-Pro.local>
2019-04-27 12:07:50 +1200

    Debug

--- client/src/actions/recordAction.js
+++ client/src/actions/recordAction.js
@@ -6,24 +6,13 @@ import UserService from '../services/user/UserService'
 export const addRecord = (recordData, history) => async dispatch => {
   // dispatch(setContentLoading())
   try {
-    const res = await RecordService.addRecord(recordData)
+    const resRecord = await RecordService.addRecord(recordData)
     dispatch(clearErrors())
-    dispatch(getUserProfile(res.data.user))
-    history.push('/user/profile')
-  } catch (err) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
+    const resUser = await UserService.getUserProfile({
+      googleToken: resRecord.data.user
     })
-  }
-}
-
-// Update user profile
-export const getUserProfile = googleToken => async dispatch => {
-  // dispatch(setContentLoading())
-  try {
-    const res = await UserService.getUserProfile({ googleToken })
-    dispatch(setCurrentUser(res.data))
+    dispatch(setCurrentUser(resUser.data))
+    history.push('/user/profile')
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
