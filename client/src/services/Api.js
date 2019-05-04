de0c6e927ff4b0bb2fa1d9a544d0c91d1b18bf39
Lindsay Xue <lindsay@Lingzis-MacBook-Pro.local>
2019-05-04 12:32:10 +1200

    Edit Api service

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
 export default () => {
+  if (process.env.NODE_ENV === 'production') {
+    return axios.create({
+      baseURL: `https://www.hifivenz.com/api/`
+    })
+  }
   return axios.create({
     baseURL: `http://localhost:5000/api/`
-    // headers: {
-    //   Authorization: `Bearer ${store.state.idToken}`
-    // }
   })
 }
