0b9b4ffb573e4d0e178d30d1a4f1ca982e7d2e83
Lindsay Xue <lindsay@Lingzis-MacBook-Pro.local>
2019-05-05 20:22:00 +1200

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db')
 const cors = require('cors')
 const Routes = require('./routes/index')
 // const passportSetup = require('./config/passport_setup')
+const path = require('path')
 
 const app = express()
 
@@ -17,6 +18,16 @@ app.use(cors())
 
 //User Routes
 app.use('/api', Routes)
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
